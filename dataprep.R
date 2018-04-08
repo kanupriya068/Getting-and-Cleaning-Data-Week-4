728184353c5c66ab72899430229ce27f5f7ad673
kanupriya068 <37248288+kanupriya068@users.noreply.github.com>
2018-04-08 15:41:27 +0530

    Add files via upload

---
+++ dataprep.R
@@ -0,0 +1,28 @@
+#base data
+activities <- read.delim("activity_labels.txt",header = FALSE,sep = "",col.names = c("Activity.ID","Activity.Name"))
+features <- read.delim("features.txt",header = FALSE,sep = "",col.names = c("Feature.ID","Feature.Name"))
+#Training Data
+Train.Sub <- read.delim("../UCI HAR Dataset/train/subject_train.txt",header = FALSE,sep = "",col.names = c("Subject.ID"))
+Train.X <- read.delim("../UCI HAR Dataset/train/X_train.txt",header = FALSE,sep = "",col.names =features$Feature.Name)
+Train.Y <- read.delim("../UCI HAR Dataset/train/y_train.txt",header = FALSE,sep = "",col.names =c("Activity.ID"))
+Train.all<- cbind(Train.Sub,Train.Y,Train.X)
+#Test Data
+Test.Sub <- read.delim("../UCI HAR Dataset/test/subject_test.txt",header = FALSE,sep = "",col.names = c("Subject.ID"))
+Test.Y <- read.delim("../UCI HAR Dataset/test/y_test.txt",header = FALSE,sep = "",col.names =c("Activity.ID"))
+Test.X <- read.delim("../UCI HAR Dataset/test/X_test.txt",header = FALSE,sep = "",col.names =features$Feature.Name)
+Test.all<- cbind(Test.Sub,Test.Y,Test.X)
+#Merge Test & Train
+Data <-rbind(Train.all,Test.all) %>% inner_join(activities,by="Activity.ID")
+#Naming activities in the dataset
+Data <- Data %>% select(Subject.ID, Activity.Name, contains("mean.."), contains("std.."))
+Data <- Data %>% setNames(gsub("Acc", "Accelerometer", names(.)))
+Data <- Data %>% setNames(gsub("Gyro", "Gyroscope", names(.)))
+Data <- Data %>% setNames(gsub("^f", "FrequencyDomain", names(.)))
+Data <- Data %>% setNames(gsub("Mag", "Magnitude", names(.))) %>% setNames(gsub("^t", "TimeDomain", names(.)))
+Data <- Data %>% setNames(gsub("mean\\.\\.", "Mean", names(.))) %>% setNames(gsub("std\\.\\.", "Std", names(.))) %>% setNames(gsub("std\\.\\.", "Std", names(.)))
+Data <- Data %>%select(-starts_with("angle"))
+#Summary
+summary1 <- Data %>% group_by(Subject.ID, Activity.Name)
+summary1 <- summary1 %>% summarize_each(funs(mean))
+write.table(summary1,file="summary-data.txt",row.name=FALSE)
+print(summary1)
